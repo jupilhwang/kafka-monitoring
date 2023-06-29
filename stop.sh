c26cd3a53e701fdb659e1c5476b8c7172cdba54a
Jupil Hwang <jupil.hwang@gmail.com>
2023-06-29 10:39:30 +0000

    add nginx proxy

--- stop.sh
+++ stop.sh
@@ -4,4 +4,6 @@
 sudo systemctl stop otelcol-contrib.service
 docker-compose down -v
 sudo rm -rf /opt/monitoring/prometheus/data/*
+sudo rm -rf /opt/monitoring/grafana/data/*
+sudo rm -rf /opt/monitoring/tempo/*
 
