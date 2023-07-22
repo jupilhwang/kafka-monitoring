192b7f11e39fac98ffab7958be3fd424d1272e49
Jupil Hwang <jupil.hwang@gmail.com>
2023-07-22 12:03:43 +0000

    clickhouse & qryn

--- stop.sh
+++ stop.sh
@@ -6,6 +6,7 @@ docker-compose down -v
 sudo rm -rf /opt/monitoring/prometheus/data/*
 sudo rm -rf /opt/monitoring/grafana/data/*
 sudo rm -rf /opt/monitoring/tempo/*
-sudo rm -rf /opt/monitoring/opensearch/data1/*
-sudo rm -rf /opt/monitoring/opensearch/data2/*
 
+
+
+sudo rm -rf /opt/monitoring/clickhouse/*
