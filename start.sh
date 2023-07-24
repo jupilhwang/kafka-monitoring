81557bba0e49323b89278d8cb3ed918895f7a77a
jupilhwang <jupil.hwang@gmail.com>
2023-07-24 01:56:03 +0000

    otel-collector

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if[[ -z /opt/monitoring/grafana/data ]]; sudo mkdir -p /opt/monitoring/grafana/data
+if[[ -z /opt/monitoring/clickhouse ]]; sudo mkdir -p /opt/monitoring/clickhouse
+
 docker-compose up -d
 #sudo systemctl start otelcol-contrib.service
 #journalctl -f -u otelcol-contrib.service
